12c6a38d0db9ce25e5c7723490fc200ffbdc4a43
immanuel albrecht <psy@zorgk.de>
2012-06-25 12:18:17 +0200

    added efmlApplet export script

---
+++ java/efml/src/de/tu_dresden/psy/efml/editor/EditorApplet.java
@@ -0,0 +1,38 @@
+/**
+ * EditorApplet.java, (c) 2012, Immanuel Albrecht; Dresden University of
+ * Technology, Professur für die Psychologie des Lernen und Lehrens
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.tu_dresden.psy.efml.editor;
+
+import java.applet.Applet;
+
+/**
+ * implements an interface to the webbrowser that exhibits the compiler to the
+ * editor web page
+ * 
+ * @author albrecht
+ * 
+ */
+
+public class EditorApplet extends Applet {
+
+	/**
+	 * Serialization ID
+	 */
+	private static final long serialVersionUID = 2929417487951462196L;
+
+}
